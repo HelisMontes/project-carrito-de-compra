ab77969e56eae22de52310f0465536849fc0fb85
Helis Montes <helismontes@gmail.com>
2021-06-15 16:32:55 -0500

    restructuring

--- js/app.js
+++ js/app.js
@@ -1,143 +1,133 @@
-System.register(["../js/data.js"], function (exports_1, context_1) {
-    "use strict";
-    var data_js_1, variables, cursosCarrito, insertHTML, cargarData, cargarEventos, seleccionarCrusos, eliminarCursos, cursoSeleccionado, updateDataCart, insertDataCart, insterHTML_Cart, syncStorage, vaciarCarrito;
-    var __moduleName = context_1 && context_1.id;
-    return {
-        setters: [
-            function (data_js_1_1) {
-                data_js_1 = data_js_1_1;
-            }
-        ],
-        execute: function () {
-            variables = {
-                carrito: document.querySelector('#carrito'),
-                listaCarrito: document.querySelector('#lista-carrito tbody'),
-                btnVaciarCarrito: document.querySelector('#vaciar-carrito'),
-                listaCursos: document.querySelector('#lista-cursos')
-            };
-            cursosCarrito = [];
-            insertHTML = (data) => {
-                const { id, img, titulo, autor, calificacion, precio, preciofinal } = data;
-                const lastrow = variables.listaCursos.lastChild;
-                lastrow.appendChild(document.createElement("DIV")).setAttribute("class", "four columns");
-                const columns = variables.listaCursos.lastChild.lastChild;
-                columns.innerHTML = `
-        <div class="card">
-            <img src="${img}" class="imagen-curso u-full-width">
-            <div class="info-card">
-                <h4>${titulo}</h4>
-                <p>${autor}</p>
-                <img src="${calificacion}">
-                <p class="precio">$${precio}<span class="u-pull-right ">$${preciofinal}</span></p>
-                <a href="#" class="u-full-width button-primary button input agregar-carrito" data-id="${id}">Agregar Al Carrito</a>
-            </div>
-        </div> <!--.card-->
-        `;
-            };
-            cargarData = () => {
-                data_js_1.data.map((res, index) => {
-                    if (index % 3 == 0) {
-                        variables.listaCursos.appendChild(document.createElement("DIV")).setAttribute("class", "row");
-                        insertHTML(res);
-                    } else {
-                        insertHTML(res);
-                    }
-                });
-            };
-            cargarEventos = () => {
-                // Dispara cuando se presiona "Agregar Carrito"
-                variables.listaCursos.addEventListener('click', seleccionarCrusos);
-                // Eliminar un curso del carrito
-                variables.carrito.addEventListener('click', eliminarCursos);
-                //Cargar los cursos de localStorage
-                window.addEventListener("load", (event) => {
-                    cursosCarrito = JSON.parse(localStorage.getItem('carrito')) || []; // Si no hay registro en localStorage asigno un registro vació
-                    insterHTML_Cart();
-                });
-                // Vaciar el carrito
-                variables.btnVaciarCarrito.addEventListener('click', () => {
-                    cursosCarrito = [];
-                    vaciarCarrito();
-                });
-            };
-            seleccionarCrusos = (e) => {
-                e.preventDefault();
-                // Selecciona el objeto que acciona el evento click
-                if (e.target.classList.contains('agregar-carrito')) {
-                    const curso = e.target.parentElement.parentElement;
-                    cursoSeleccionado(curso);
-                }
-            };
-            eliminarCursos = (e) => {
-                e.preventDefault();
-                if (e.target.classList.contains('borrar-curso')) {
-                    const idCurso = e.target.getAttribute('data-id');
-                    // Eliminar del arreglo del carrito
-                    cursosCarrito = cursosCarrito.filter(curso => curso.id !== idCurso);
-                    insterHTML_Cart();
-                }
-            };
-            cursoSeleccionado = (curso) => {
-                const objCurso = {
-                    id: curso.querySelector('a').getAttribute('data-id'),
-                    img: curso.querySelector('img').src,
-                    titulo: curso.querySelector('h4').textContent,
-                    precio: curso.querySelector('.precio span').textContent.substr(1),
-                    cantidad: 1
-                }; //Datos del curso actual
-                const existe = cursosCarrito.some(curso => curso.id === objCurso.id); //Valido si existe el curso
-                existe ? updateDataCart(objCurso) : insertDataCart(objCurso);
-            };
-            updateDataCart = (objCurso) => {
-                const sumarCantidad = cursosCarrito.map(curso => {
-                    if (curso.id === objCurso.id) {
-                        curso.cantidad++;
-                        curso.precio = Number(curso.precio) + Number(objCurso.precio);
-                        return curso; //Retorna el Objeto actualizado
-                    }
-                    else {
-                        return curso; //Retorna los Objetos que no son actualizado
-                    }
-                });
-                cursosCarrito = [...sumarCantidad];
-                insterHTML_Cart();
-            };
-            insertDataCart = (objCurso) => {
-                cursosCarrito = [...cursosCarrito, objCurso];
-                insterHTML_Cart();
-            };
-            insterHTML_Cart = () => {
-                vaciarCarrito();
-                cursosCarrito.map(cursos => {
-                    const { img, titulo, precio, cantidad, id } = cursos;
-                    const row = document.createElement('tr');
-                    row.innerHTML = `
-            <td>  
-                <img src="${img}" width=100>
-            </td>
-            <td>${titulo}</td>
-            <td>$${precio}</td>
-            <td>${cantidad} </td>
-            <td>
-                <a href="#" class="borrar-curso" data-id="${id}">X</a>
-            </td>  
-        `;
-                    variables.listaCarrito.appendChild(row);
-                });
-                syncStorage();
-            };
-            syncStorage = () => {
-                localStorage.setItem('carrito', JSON.stringify(cursosCarrito));
-            };
-            vaciarCarrito = () => {
-                // contenedorCarrito.innerHTML = ''; // forma lenta
-                // forma rapida (recomendada)
-                while (variables.listaCarrito.firstChild) {
-                    variables.listaCarrito.removeChild(variables.listaCarrito.firstChild);
-                }
-            };
-            cargarData();
-            cargarEventos();
-        }
-    };
-});
+import {data} from './data.js';
+
+const variables = {
+  carrito: document.querySelector('#carrito'),
+  listaCarrito: document.querySelector('#lista-carrito tbody'),
+  btnVaciarCarrito: document.querySelector('#vaciar-carrito'),
+  listaCursos: document.querySelector('#lista-cursos')
+}
+let cursosCarrito = [];
+
+const insertHTML = (data) => {
+  const { id, img, titulo, autor, calificacion, precio, preciofinal } = data;
+    const lastrow = variables.listaCursos.lastChild;
+    lastrow.appendChild(document.createElement("DIV")).setAttribute("class", "four columns")
+    const columns = variables.listaCursos.lastChild.lastChild
+    columns.innerHTML = `
+      <div class="card">
+        <img src="${img}" class="imagen-curso u-full-width">
+        <div class="info-card">
+          <h4>${titulo}</h4>
+          <p>${autor}</p>
+          <img src="${calificacion}">
+          <p class="precio">$${precio}<span class="u-pull-right ">$${preciofinal}</span></p>
+          <a href="#" class="u-full-width button-primary button input agregar-carrito" data-id="${id}">Agregar Al Carrito</a>
+        </div>
+      </div>`;
+}
+
+const cargarData = () => {
+  data.map((res, index)=>{
+    if(index % 3 == 0){
+      variables.listaCursos.appendChild(document.createElement("DIV")).setAttribute("class", "row");
+      insertHTML(res);
+    }else {
+      insertHTML(res);
+    }
+  });
+}
+
+const cargarEventos = () => {
+  variables.listaCursos.addEventListener('click', seleccionarCrusos);
+  variables.carrito.addEventListener('click', eliminarCursos)
+  window.addEventListener("load", (event) => {
+    cursosCarrito = JSON.parse( localStorage.getItem('carrito')) || [] // Si no hay registro en localStorage asigno un registro vació
+    insterHTML_Cart();
+  });
+  variables.btnVaciarCarrito.addEventListener('click', () => {
+    cursosCarrito = [];
+    vaciarCarrito();
+  })
+};
+
+const seleccionarCrusos = (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains('agregar-carrito')) {
+    const curso = e.target.parentElement.parentElement;
+    cursoSeleccionado(curso);
+  }
+}
+
+const eliminarCursos = (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains('borrar-curso')) {
+    const idCurso = e.target.getAttribute('data-id');
+    cursosCarrito = cursosCarrito.filter(curso => curso.id !== idCurso)
+    insterHTML_Cart();
+  }
+}
+
+const cursoSeleccionado = (curso) =>{
+  const objCurso = {
+    id: curso.querySelector('a').getAttribute('data-id'),
+    img: curso.querySelector('img').src,
+    titulo: curso.querySelector('h4').textContent,
+    precio: curso.querySelector('.precio span').textContent.substr(1),
+    cantidad: 1
+  };
+  const existe = cursosCarrito.some(curso => curso.id === objCurso.id); //Valido si existe el curso
+  existe ? updateDataCart (objCurso) : insertDataCart (objCurso); 
+}
+
+const updateDataCart = (objCurso) => {
+  const sumarCantidad = cursosCarrito.map(curso =>{
+    if (curso.id === objCurso.id){
+      curso.cantidad++;
+      curso.precio = Number(curso.precio) + Number(objCurso.precio)
+      return curso;
+    }else{
+      return curso;
+    }
+  })
+  cursosCarrito = [...sumarCantidad];
+  insterHTML_Cart()
+}
+
+const insertDataCart = (objCurso) => {
+  cursosCarrito = [...cursosCarrito, objCurso]
+  insterHTML_Cart()
+}
+
+const insterHTML_Cart = () => {
+  vaciarCarrito();
+  cursosCarrito.map( cursos => {
+    const { img, titulo, precio, cantidad, id } = cursos
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>  
+        <img src="${img}" width=100>
+      </td>
+      <td>${titulo}</td>
+      <td>$${precio}</td>
+      <td>${cantidad} </td>
+      <td>
+        <a href="#" class="borrar-curso" data-id="${id}">X</a>
+      </td>  
+    `;
+      variables.listaCarrito.appendChild(row);
+  })
+  syncStorage()
+}
+
+const syncStorage = () =>{
+  localStorage.setItem('carrito', JSON.stringify(cursosCarrito));
+}
+
+const vaciarCarrito = () =>{
+  while(variables.listaCarrito.firstChild) {
+    variables.listaCarrito.removeChild(variables.listaCarrito.firstChild);
+  }
+}
+
+cargarData();
+cargarEventos();
--- js/data.js
+++ js/data.js
@@ -1,83 +1,73 @@
-System.register([], function (exports_1, context_1) {
-    "use strict";
-    var data;
-    var __moduleName = context_1 && context_1.id;
-    return {
-        setters: [],
-        execute: function () {
-            exports_1("data", data = [{
-                    id: 1,
-                    img: "img/curso1.jpg",
-                    titulo: "HTML5, CSS3, JavaScript para Principiantes",
-                    autor: "Juan Puerta",
-                    calificacion: "img/estrellas.png",
-                    precio: 200,
-                    preciofinal: 15
-                }, {
-                    id: 2,
-                    img: "img/curso2.jpg",
-                    titulo: "Curso de Comida Vegetariana",
-                    autor: "Juan Castillo",
-                    calificacion: "img/estrellas.png",
-                    precio: 250,
-                    preciofinal: 15
-                }, {
-                    id: 3,
-                    img: "img/curso3.jpg",
-                    titulo: "Guitarra para Principiantes",
-                    autor: "Jesus Perez",
-                    calificacion: "img/estrellas.png",
-                    precio: 150,
-                    preciofinal: 15
-                }, {
-                    id: 4,
-                    img: "img/curso4.jpg",
-                    titulo: "Huerto en tu casa",
-                    autor: "Marina Paz",
-                    calificacion: "img/estrellas.png",
-                    precio: 220,
-                    preciofinal: 15
-                }, {
-                    id: 5,
-                    img: "img/curso5.jpg",
-                    titulo: "Decoración con productos de tu hogar",
-                    autor: "Angela Jimenez",
-                    calificacion: "img/estrellas.png",
-                    precio: 150,
-                    preciofinal: 15
-                }, {
-                    id: 6,
-                    img: "img/curso1.jpg",
-                    titulo: "Diseño Web Intermedio",
-                    autor: "Juan Puerta",
-                    calificacion: "img/estrellas.png",
-                    precio: 250,
-                    preciofinal: 15
-                }, {
-                    id: 7,
-                    img: "img/curso2.jpg",
-                    titulo: "Comida Mexicana para principiantes",
-                    autor: "Juan Castillo",
-                    calificacion: "img/estrellas.png",
-                    precio: 200,
-                    preciofinal: 15
-                }, {
-                    id: 8,
-                    img: "img/curso3.jpg",
-                    titulo: "Estudio Musical en tu casa",
-                    autor: "Jesus Perez",
-                    calificacion: "img/estrellas.png",
-                    precio: 100,
-                    preciofinal: 15
-                }, {
-                    id: 9,
-                    img: "img/curso5.jpg",
-                    titulo: "Prepara galletas caseras",
-                    autor: "Angela Jimenez",
-                    calificacion: "img/estrellas.png",
-                    precio: 150,
-                    preciofinal: 15
-                }]);
-        }
-    };
-});
+export const data = [{
+    id: 1,
+    img:"img/curso1.jpg",
+    titulo:"HTML5, CSS3, JavaScript para Principiantes",
+    autor:"Juan Puerta",
+    calificacion:"img/estrellas.png",
+    precio:200,
+    preciofinal:15
+},{
+    id: 2,
+    img:"img/curso2.jpg",
+    titulo:"Curso de Comida Vegetariana",
+    autor:"Juan Castillo",
+    calificacion:"img/estrellas.png",
+    precio:250,
+    preciofinal:15
+},{
+    id: 3,
+    img:"img/curso3.jpg",
+    titulo:"Guitarra para Principiantes",
+    autor:"Jesus Perez",
+    calificacion:"img/estrellas.png",
+    precio:150,
+    preciofinal:15
+},{
+    id: 4,
+    img:"img/curso4.jpg",
+    titulo:"Huerto en tu casa",
+    autor:"Marina Paz",
+    calificacion:"img/estrellas.png",
+    precio:220,
+    preciofinal:15
+},{
+    id: 5,
+    img:"img/curso5.jpg",
+    titulo:"Decoración con productos de tu hogar",
+    autor:"Angela Jimenez",
+    calificacion:"img/estrellas.png",
+    precio:150,
+    preciofinal:15
+},{
+    id: 6,
+    img:"img/curso1.jpg",
+    titulo:"Diseño Web Intermedio",
+    autor:"Juan Puerta",
+    calificacion:"img/estrellas.png",
+    precio:250,
+    preciofinal:15
+},{
+    id: 7,
+    img:"img/curso2.jpg",
+    titulo:"Comida Mexicana para principiantes",
+    autor:"Juan Castillo",
+    calificacion:"img/estrellas.png",
+    precio:200,
+    preciofinal:15
+},{
+    id: 8,
+    img:"img/curso3.jpg",
+    titulo:"Estudio Musical en tu casa",
+    autor:"Jesus Perez",
+    calificacion:"img/estrellas.png",
+    precio:100,
+    preciofinal:15
+},{
+    id: 9,
+    img:"img/curso5.jpg",
+    titulo:"Prepara galletas caseras",
+    autor:"Angela Jimenez",
+    calificacion:"img/estrellas.png",
+    precio:150,
+    preciofinal:15
+}]
